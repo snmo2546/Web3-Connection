0e4c43cb948d5a92bc1a48b5af31ce7c991bcd87
jacky-senkusha <jacky@senkusha.xyz>
2023-05-11 15:00:13 +0800

    feat: include express-handlebars

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
+
 const app = express()
 const port = 3000
+
+app.engine('hbs', engine({extname: '.hbs'}))
+app.set('view engine', 'hbs')
+app.set('views', './views')
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
 
 app.listen(port, () => {
     console.log(`Web3 Connection listening on port ${port}`)
