6af9cd5881a3dbf2f51e47465fefe0b259ce1524
jacky-senkusha <jacky@senkusha.xyz>
2023-05-11 16:24:42 +0800

    feat: get user address from metamask

--- public/javascripts/connect.js
+++ public/javascripts/connect.js
@@ -1,1 +1,17 @@
-console.log(document.getElementById('connectButton'))
+const connectButton = document.getElementById('connectButton')
+
+connectButton.addEventListener('click', async () => {
+    try {
+        await window.ethereum.enable()
+
+        const web3 = new Web3(window.ethereum)
+
+        const accounts = await web3.eth.getAccounts()
+        const userAddress = accounts[0]
+
+        console.log('Connected to MetaMask')
+        console.log('User Address:', userAddress)
+    } catch (error) {
+        console.log(error)
+    }
+})
